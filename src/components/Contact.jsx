1f240b6a0d8eaf86b8c4652353c76594aba23076
s <s>
2021-05-11 10:53:52 -0300

    added contactme componen with the styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,78 @@
 import React from 'react';
 
 function Contact() {
+
+
+
+
   return (
     <div id='Contact' >
-        contacto
+        
+        
+    
+
+<form>
+  {/* inputs formularios */}
+    <div class="form-group m-5">
+
+      {/* Title */}
+    <h3 className='PortfolioTitle'>Contactame!!</h3>
+      {/* /Title */}
+
+
+{/* Email */}
+      <div className='m-3'>
+      <input type="email" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Ingresa tu email"/>
+      </div>
+{/* /Email */}
+
+
+
+{/* Telefono */}
+    <div className='m-3'>
+    <input type="text" class="form-control" id="Telefono"  placeholder="Ingresa tu telefono"/>
+    </div>
+{/* Telefono */}
+
+
+
+{/* Nombre completo */}
+<div className='m-3'>
+    <input type="text" class="form-control" id="Nombre completo"  placeholder="Ingresa tu nombre completo"/>
+    </div>
+{/* Nombre completo */}
+
+
+
+{/* Nombre completo */}
+<div className='m-3'>
+    <textarea cols='25'  class="form-control" id="Nombre completo"  placeholder="Escribe un mensaje"/>
+    </div>
+{/* Nombre completo */}
+
+
+
+{/* Button */}
+<div class="d-flex justify-content-end">
+
+    <button class="btn btn-secondary m-3" >Success</button>  
+    </div>
+{/* Button */}
+
+
+      </div>
+  {/* inputs formularios */}
+
+
+
+
+
+</form>
+
+
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
     </div>
   );
 }
